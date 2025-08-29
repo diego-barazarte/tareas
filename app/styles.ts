ebb94e47cde14fd671c353378b6c3a50a124cb5d
Diego Barazarte <diegob.betancourt@gmail.com>
2025-08-28 20:16:27 -0500

    Se cambió el color de fondo del modal

--- app/styles.ts
+++ app/styles.ts
@@ -59,7 +59,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    backgroundColor: 'rgba(52, 58, 64, 0.6)',
   },
   modalContent: {
     backgroundColor: '#fff',
